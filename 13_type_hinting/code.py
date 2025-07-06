8d9c1a35ab47cbf9bf458181c298762116af1872
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-07-06 13:34:18 +0800

    Add Type Hinting

---
+++ 13_type_hinting/code.py
@@ -0,0 +1,36 @@
+# Your code tells you whether you've passed in the wrong data type in your custom functions and classes
+
+# ----- Functions -----
+# Params: LIST
+# Return: FLOAT
+def list_avg(sequence: list) -> float:
+    return sum(sequence) / len(sequence)
+
+# ----- Classes -----
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name: str, book_type: str, weight: int):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self) -> str:
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name: str, page_weight: int) -> "Book": # CUSTOM
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+    @classmethod
+    def paperback(cls, name: str, page_weight: int) -> "Book":
+        return cls(name, cls.TYPES[1], page_weight)
+
+# Param: LIST with elements of datatype Book
+from typing import List
+class BookShelf:
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self) -> str:
+        return f"BookShelf with {len(self.books)} books."
