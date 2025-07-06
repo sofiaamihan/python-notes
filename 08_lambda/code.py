ccfe6a49e9c94483d8c5179db4db394f215c9434
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-07-06 11:11:29 +0800

    Add Lambda Functions

---
+++ 08_lambda/code.py
@@ -0,0 +1,25 @@
+# Long Version
+def add(x, y):
+    return x + y
+print(add(5,3))
+
+# Short Version
+subtract = lambda x, y: x-y
+print(subtract(5, 3))
+
+# Syntax
+# lambda params : return value
+
+test = [1, 2, 3, 4, 5]
+
+# Practice 1
+# Create a lambda function that will square each value in this list
+square = lambda x: x**2
+new_list = [square(result) for result in test]
+print(new_list)
+
+# Practice 2
+# Use map()
+# map() executes a specified function for each item in the list
+new_list2 = list(map(lambda x:x**2, test))
+print(new_list2)
