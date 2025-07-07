6db1202bad05df27f570d5ee3bc9f5d259c3fe62
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-07-07 14:17:58 +0800

    Update Decorators with Parameters

--- 20_decorators_parameters/code.py
+++ 20_decorators_parameters/code.py
@@ -6,21 +6,21 @@ user = {
     "access_level": "guest"
 }
 
-def denied_password():
-    return "You do not have access"
-
-# This is called a decorator
-def make_secure(func):
-    @functools.wraps(func)
-    def secure_function(*args, **kwargs):
-        if user["access_level"] == "admin":
-            return func(*args, **kwargs)
-        else:
-            return "You do not have access."
-    return secure_function
+# This is a factory, that makes a decorator
+def make_secure(access_level):
+    # This is called a decorator
+    def decorator(func):
+        @functools.wraps(func)
+        def secure_function(*args, **kwargs):
+            if user["access_level"] == access_level:
+                return func(*args, **kwargs)
+            else:
+                return "You do not have access."
+        return secure_function
+    return decorator
 
 # This has been decorated
-@make_secure
+@make_secure("admin")
 def get_password(panel):
     if panel == "admin":
         return "1234"
