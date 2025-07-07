54bb859174973cbe9625c7e92e728131da80c9e4
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-07-07 14:15:26 +0800

    Creating Dynamic Decorators

--- 20_decorators_parameters/code.py
+++ 20_decorators_parameters/code.py
@@ -1,1 +1,31 @@
-print("filler")
+# Making this dynamic
+import functools
+
+user = {
+    "username": "john",
+    "access_level": "guest"
+}
+
+def denied_password():
+    return "You do not have access"
+
+# This is called a decorator
+def make_secure(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if user["access_level"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            return "You do not have access."
+    return secure_function
+
+# This has been decorated
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "5678"
+
+
+print(get_password("billing"))
