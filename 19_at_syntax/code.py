4ad6fad391e5dd9dc299d72e9d982494c2adcb36
sofiaamihan <sofiaamihanmrespeto@gmail.com>
2025-07-07 14:10:22 +0800

    Update @ Syntax

--- 19_at_syntax/code.py
+++ 19_at_syntax/code.py
@@ -1,4 +1,8 @@
 # @ - ensures that the code does not accidentally run before the one specified at @
+
+import functools
+# Need to specify it's a wrap otherwise the name and documentation of get_password will be overwritten
+
 user = {
     "username": "john",
     "access_level": "guest"
@@ -8,10 +12,13 @@ def denied_password():
     return "You do not have access"
 
 def make_secure(func):
-    if user["access_level"] == "admin":
-        return func
-    else:
-        return denied_password
+    @functools.wraps(func)
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func
+        else:
+            return denied_password
+    return secure_function
 
 @make_secure
 def get_password():
